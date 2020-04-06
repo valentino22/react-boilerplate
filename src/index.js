b6bc731d3ab15ddf4d5c8a2102a9f075b8e5cfca
Szabolcs Ferenczi <szabolcs.ferenczi@aerlingus.com>
2020-04-06 13:31:25 +0100

    Adding react and preset for webpack to handle jsx.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-import getGreeting from './greet';
-console.log(getGreeting('world'));
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App'
+
+ReactDom.render(<App/>, document.getElementById('app'))
