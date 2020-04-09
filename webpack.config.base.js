89d9aaf9e5f52e92d86f2b450f70767e400e6ffc
Szabolcs Ferenczi <szabolcs.ferenczi@aerlingus.com>
2020-04-09 17:24:25 +0100

    - Added webpack-dev-server for hot reload. - Extending component with state. - Added source-map for debugging - Added webpack-merge for dev and prod configs

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
@@ -15,7 +14,8 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         options:{
-          presets: ['@babel/preset-env', '@babel/preset-react']
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-proposal-class-properties']
         }
       }
     ]
